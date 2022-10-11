9b1d22e38f41e831d55a0f919d7104e6f0e404cc
Zachary Yang <113367705+Zachyang02@users.noreply.github.com>
2022-10-11 12:37:27 -0400

    Update toplevel.vhd

--- Project1/VHDL_Code/toplevel.vhd
+++ Project1/VHDL_Code/toplevel.vhd
@@ -10,21 +10,28 @@ end entity ro_puf;
 
 architecture reset of ro_puf is 
 begin
-process is 
+demonstration: process is 
     
-assert reset = --;
+assert reset = -- need reset value
 
 --provide challenge to the ro_puf entity (still need this part of code)
 
 end process;
 end architecture;
 
-entity ro_puf is
+architecture enable of ro_puf is 
 begin
-
-process
-assert enable = --;
-wait for probe_delay µs; 
+demonstration: process is
+    variable out_line: line;
+  begin
+     for i in 0 to 10 loop -- not sure what range loop is. 13? 
+			write(out_line, "now looping on number " & integer'image(i));
+			writeline(output, out_line);
+			-- the condition must be false for the assertion to trigger!
+			assert enable /= 3 report "we have reached value 3" severity note; --need to change assert value
+			wait for 1 ns;
+		end loop; 
+wait for probe_delay µs; --need probe delay
 
 
 
