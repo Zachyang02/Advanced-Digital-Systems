8921e78ce1538416a2ab29a92609e41f52d7600d
Isra Chokwatana <113561482+Isra3750@users.noreply.github.com>
2022-10-23 21:29:45 -0400

    Add files via upload

---
+++ Project 2/Mandelbrot_gen.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity gen is
+    port (
+
+    );
+end entity gen;
+
+--In the architecture of the body, you should use a generate statement
+
+architecture mgen of Mandelbrot_gen is
+    -- signals
+	 
+begin
+
+	 
+end architecture mgen;
