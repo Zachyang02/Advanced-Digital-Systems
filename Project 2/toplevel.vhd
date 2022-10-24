326b3f4f1ad238ab003d66767217f64b7610472f
Isra Chokwatana <113561482+Isra3750@users.noreply.github.com>
2022-10-23 20:02:38 -0400

    Add files via upload

--- Project 2/toplevel.vhd
+++ Project 2/toplevel.vhd
@@ -21,22 +21,28 @@ entity toplevel is
 end entity toplevel;
 
 architecture arch of toplevel is
-
+	-- signals
+	signal clock: 	std_logic; -- not sure if this signal is even needed
 begin
 	-- output, incomplete right now
 	vfsm: vga_fsm
-		port (
+		port map (
 			
-			vga_clock:		=> clock
-			reset:			=> reset
+			vga_clock		=> clock,
+			reset			=> reset,
 
-			point:			=> -- add
-			point_valid:	=> -- add
+			point			=> ,
+			point_valid	=> ,
 
-			h_sync:			=> h_sync_out;
-			v_sync:			=> v_sync_out;
+			h_sync			=> h_sync_out,
+			v_sync			=> v_sync_out
 		);
 	-- make an instance of pll, inclk takes 50 Mhz (from clock) , clk output to all other designs
+	pll_inst : pll PORT MAP (
+		inclk0	 => clock,
+		c0	 => clock_out -- what signal here
+	);
+	
 	-- color output transmit data to screen, compute set first, then draw it, to draw we send color data out of the 12 color lines
 
 end architecture arch;
