fc74d83340d5ab98c2ac931ae8ccca5eee74cfd5
Zachary Yang <113367705+Zachyang02@users.noreply.github.com>
2022-10-25 16:07:55 -0400

    Add files via upload

---
+++ test/zach quartus vlabs/Mandelbrot_gen.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity gen is
+    port (
+
+    );
+end entity gen;
+
+architecture mgen of Mandelbrot_gen is
+    -- signals
+	 
+begin
+
+	 
+end architecture mgen;
