c704cd7af909f7bd03c961a8e31c77264aa84ce6
Brett Richard <100238129+brettr12321@users.noreply.github.com>
2022-10-25 21:48:20 -0400

    Update Mandelbrot_gen.vhd

--- Project 2/Mandelbrot_gen.vhd
+++ Project 2/Mandelbrot_gen.vhd
@@ -2,7 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity gen is
-    port (
+    port (		in_z: in ads_complex;
+			in_c: in ads_complex;
+			in_ou: in std_logic;
+			in_stage: in natural;
+			
+			out_z: out ads_complex;
+			out_c: out ads_complex;
+			out_ou: out std_logic;
+			out_stage: out natural
 
     );
 end entity gen;
@@ -15,22 +23,15 @@ architecture mgen of Mandelbrot_gen is
 begin
 
 
-function algorithm1 (c	: ads_complex:
-				iterations	: natural )
-	return ads_complex is variable result; 
+z.re <= z.re * z.re;
+z.im <= z.im * z.im;
+z <= z.im * z.re;
+out_ou <= '1' if in_ou = '1' else abs2(z) > 2; --error on this line
+out_c <= in_c;
+out_stage <= in_stage when in_ou = '1' else stage_number;
 
-		begin
-			 z <= 0;
-			iteration <= 0;
-			
-			while iteration < iterations loop 
-				z <= (z * z) + c ;
-					if abs2(z) > 2 then
-						exit;
-					end if;
-				 iteration <= iteration + 1;
-				--Return something
-			end while;
+	
+	
 end function algorithm1;
 	 
 end architecture mgen;
