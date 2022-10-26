350f22dc82a688abec9f80a23813db4b6e54f48e
Isra Chokwatana <113561482+Isra3750@users.noreply.github.com>
2022-10-26 00:23:16 -0400

    Add files via upload

--- Project 2/Mandelbrot_gen.vhd
+++ Project 2/Mandelbrot_gen.vhd
@@ -1,37 +1,38 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity gen is
-    port (		in_z: in ads_complex;
+library ads;
+use ads.ads_fixed.all;
+use ads.ads_complex_pkg.all;
+
+entity pipeline_stage is
+	generic (
+		threshold: ads_sfixed := to_ads_sfixed(2);
+		stage_number: natural := 0
+	);
+
+    port (		
+			in_z: in ads_complex;
 			in_c: in ads_complex;
-			in_ou: in std_logic;
+			in_ov: in std_logic;
 			in_stage: in natural;
 			
 			out_z: out ads_complex;
 			out_c: out ads_complex;
-			out_ou: out std_logic;
+			out_ov: out std_logic;
 			out_stage: out natural
-
     );
-end entity gen;
+end entity pipeline_stage ;
 
---In the architecture of the body, you should use a generate statement
-
-architecture mgen of Mandelbrot_gen is
-    -- signals
-	 
+architecture mgen of pipeline_stage is
 begin
 
+	z.re <= z.re * z.re;
+	z.im <= z.im * z.im;
+	z <= z.im * z.re;
+	out_ov <= '1' when (in_ov = '1' or abs2(z) > to_ads_sfixed(2)) else '0';
 
-z.re <= z.re * z.re;
-z.im <= z.im * z.im;
-z <= z.im * z.re;
-out_ou <= '1' if in_ou = '1' else abs2(z) > 2; --error on this line
-out_c <= in_c;
-out_stage <= in_stage when in_ou = '1' else stage_number;
+	out_c <= in_c;
+	out_stage <= in_stage when in_ov = '1' else stage_number;	
 
-	
-	
-
-	 
 end architecture mgen;
--- Project 2/toplevel.vhd
+++ Project 2/toplevel.vhd
@@ -4,6 +4,9 @@ use ieee.std_logic_1164.all;
 library work;
 use work.project_pkg.all;
 
+library vga;
+use vga.vga_data.all;
+
 entity toplevel is
 	port (		
 		reset:		in	std_logic;
@@ -20,19 +23,31 @@ entity toplevel is
 	
 end entity toplevel;
 
+-- add to libary, goto properties, libary -> ads (ads_fixed, ads_complex)/ vga (vga_data/vga_fsm)
 architecture arch of toplevel is
+	-- components
+	component pll
+	PORT
+	(
+		inclk0		: IN STD_LOGIC  := '0';
+		c0		: OUT STD_LOGIC 
+	);
+	end component pll;
+	
 	-- signals
-	signal clock: 	std_logic; -- not sure if this signal is even needed
+  	signal point_bool: 	boolean;
+	signal clock_out: std_logic; -- not sure of signal type
+	signal point_current: coordinate; 
 begin
 	-- output, incomplete right now
 	vfsm: vga_fsm
 		port map (
 			
-			vga_clock		=> clock,
+			vga_clock		=> clock_out,
 			reset			=> reset,
 
-			point			=> ,
-			point_valid	=> ,
+			point			=> point_current,
+			point_valid	=> point_bool,
 
 			h_sync			=> h_sync_out,
 			v_sync			=> v_sync_out
@@ -40,9 +55,12 @@ begin
 	-- make an instance of pll, inclk takes 50 Mhz (from clock) , clk output to all other designs
 	pll_inst : pll PORT MAP (
 		inclk0	 => clock,
-		c0	 => clock_out -- what signal here
+		c0	 => clock_out 
 	);
 	
 	-- color output transmit data to screen, compute set first, then draw it, to draw we send color data out of the 12 color lines
-
+	red_out <= "0000";
+	green_out <= "1111" when point_bool else "0000";
+	blue_out <= "0000";
+	
 end architecture arch;
